0bfe882dda8633d36dc2257d398dfb29f3096faf
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2013-05-22 22:09:44 +0300

    fixed formatting

--- src/hamakar/circular_buffer.clj
+++ src/hamakar/circular_buffer.clj
@@ -3,20 +3,20 @@
 (defn- default-value
   [t]
   (case t
-	  :any nil
-	  :boolean false
-	  0))
+    :any nil
+    :boolean false
+    0))
 
 (defn- empty-vector
   [t size default]
-	 (if (= t :any)
-	     (vec (repeat size default))
-	     (into (vector-of t) (repeat size default))))
+  (if (= t :any )
+    (vec (repeat size default))
+    (into (vector-of t) (repeat size default))))
 
 (defn- ^long circular-nth
   [^long i ^long start ^long size direction]
   (let [offset (if (= :left direction) 0 1)]
-	  (rem (+ start i offset) size)))
+    (rem (+ start i offset) size)))
 
 (declare circular-previous)
 
@@ -24,99 +24,99 @@
   [^long start ^long size direction]
   (if (= :left direction)
     (rem (inc start) size)
-    (circular-previous start size :left)))
+    (circular-previous start size :left )))
 
 (defn- ^long circular-previous
   [^long start ^long size direction]
   (if (= :left direction)
     (rem (+ start size -1) size)
-    (circular-next start size :left)))
+    (circular-next start size :left )))
 
 (deftype CircularBuffer [^clojure.lang.IPersistentVector items
                          ^int size ^int start
                          ^:unsynchronized-mutable ^int hashcode
-                          type default direction _meta]
-  
+                         type default direction _meta]
+
   Object
   (equals [this o]
     (cond
       (identical? this o) true
       (instance? CircularBuffer o)
       (and
-        (= size      (.size ^CircularBuffer o))
-        (= start     (.start ^CircularBuffer o))
+        (= size (.size ^CircularBuffer o))
+        (= start (.start ^CircularBuffer o))
         (= direction (.direction ^CircularBuffer o))
-        (= items     (.items ^CircularBuffer o)))
+        (= items (.items ^CircularBuffer o)))
       :else false))
   (hashCode [_]
     (when (== -1 hashcode)
       (set! hashcode (->> (int 17)
-										      (unchecked-multiply-int 31)
-										      (unchecked-add-int start)
-                          (unchecked-multiply-int 31)
-										      (unchecked-add-int (clojure.lang.Util/hash direction))
-										      (unchecked-multiply-int 31)
-										      (unchecked-add-int (clojure.lang.Util/hash items)))))
+                       (unchecked-multiply-int 31)
+                       (unchecked-add-int start)
+                       (unchecked-multiply-int 31)
+                       (unchecked-add-int (clojure.lang.Util/hash direction))
+                       (unchecked-multiply-int 31)
+                       (unchecked-add-int (clojure.lang.Util/hash items)))))
     hashcode)
-  
-  
+
+
   clojure.lang.Counted
   (count [_] size)
-  
+
   clojure.lang.IHashEq
   (hasheq [self] (hash self))
-  
+
   clojure.lang.IMeta
   (meta [_] _meta)
 
   clojure.lang.IObj
   (withMeta [_ m] (CircularBuffer. items size start hashcode type default direction m))
-  
+
   clojure.lang.Indexed
   (nth [_ i]
     (let [pos (circular-nth i start size direction)]
       (nth items pos)))
   (nth [this i not-found]
-     (let [z (int 0)]
-       (if (and (>= i z) (< i size))
-		       (.nth this i)
-		       not-found)))
-  
+    (let [z (int 0)]
+      (if (and (>= i z) (< i size))
+        (.nth this i)
+        not-found)))
+
   clojure.lang.IPersistentCollection
   (cons [this val]
     (let [new-items (assoc items start val)]
       (CircularBuffer. new-items size (circular-next start size direction) -1 type default direction (meta this))))
   (empty [this]
-    (CircularBuffer. (empty-vector type size default) size 0 -1 type default direction (meta this)))  
+    (CircularBuffer. (empty-vector type size default) size 0 -1 type default direction (meta this)))
   (equiv [this o]
     (cond
       (or (instance? clojure.lang.IPersistentVector o) (instance? java.util.RandomAccess o))
-        (and (= size (count o))
-             (loop [i (int 0)]
-               (cond
-	               (= i size) true
-	               (= (.nth this i) (nth o i)) (recur (inc i))
-	               :else false)))
+      (and (= size (count o))
+        (loop [i (int 0)]
+          (cond
+            (= i size) true
+            (= (.nth this i) (nth o i)) (recur (inc i))
+            :else false)))
       (or (instance? clojure.lang.Sequential o) (instance? java.util.List o))
-        (clojure.lang.Util/equiv (seq this) (seq o))
+      (clojure.lang.Util/equiv (seq this) (seq o))
       :else false))
-  
+
   clojure.lang.IPersistentStack
   (peek [this]
     (when (> size (int 0))
       (.nth this (if (= :right direction)
-                     0
-                     (dec size)))))
+                   0
+                   (dec size)))))
   (pop [this]
     (let [prv-start (circular-previous start size direction)
           new-items (assoc items prv-start default)]
       (CircularBuffer. new-items size prv-start -1 type default direction (meta this))))
-  
+
   clojure.lang.IPersistentVector
   (assocN [this i val]
     (let [new-items (assoc items (circular-nth i start size direction) val)]
       (CircularBuffer. new-items size start -1 type default direction (meta this))))
-  
+
   clojure.lang.Associative
   (assoc [this k v]
     (if (clojure.lang.Util/isInteger k)
@@ -124,19 +124,19 @@
       (throw (IllegalArgumentException. "Key must be integer"))))
   (containsKey [this k]
     (and (clojure.lang.Util/isInteger k)
-         (<= 0 (int k))
-         (< (int k) size)))
+      (<= 0 (int k))
+      (< (int k) size)))
   (entryAt [this k]
     (when (.containsKey this k)
       (clojure.lang.MapEntry. k (.nth this (int k)))))
-  
+
   clojure.lang.ILookup
   (valAt [this k not-found]
     (if (clojure.lang.Util/isInteger k)
-        (.nth this (int k))
-        not-found))
+      (.nth this (int k))
+      not-found))
   (valAt [this k] (.valAt this k nil))
-  
+
   clojure.lang.IFn
   (invoke [this k]
     (if (clojure.lang.Util/isInteger k)
@@ -145,14 +145,14 @@
           (.nth this i)
           (throw (IndexOutOfBoundsException.))))
       (throw (IllegalArgumentException. "Key must be integer"))))
-  
+
   clojure.lang.Seqable
   (seq [this]
     (for [i (range size)]
       (.nth this i)))
 
   clojure.lang.Sequential
-  
+
   java.lang.Iterable
   (iterator [this]
     (let [i (java.util.concurrent.atomic.AtomicInteger. 0)]
@@ -160,7 +160,7 @@
         (hasNext [_] (< (.get i) size))
         (next [_] (.nth this (dec (.incrementAndGet i))))
         (remove [_] (throw (UnsupportedOperationException.))))))
-  
+
   java.util.Collection
   (contains [_ o] (boolean (some #(= % o) items)))
   (containsAll [this c] (every? #(.contains this %) c))
@@ -180,7 +180,7 @@
   (^boolean remove [_ o] (throw (UnsupportedOperationException.)))
   (removeAll [_ c] (throw (UnsupportedOperationException.)))
   (retainAll [_ c] (throw (UnsupportedOperationException.)))
-)
+  )
 
 (defmethod print-method CircularBuffer [v w]
   ((get (methods print-method) clojure.lang.IPersistentVector) (seq v) w))
@@ -191,8 +191,8 @@
   :double :byte :short :char or :boolean."
   [size & {:keys [type direction default]
            :or {type :any direction :left default :type-default}}]
-  (let [def-val  (if (= default :type-default)
-                     (default-value type)
-                      default)
+  (let [def-val (if (= default :type-default )
+                  (default-value type)
+                  default)
         init-vec (empty-vector type size def-val)]
     (CircularBuffer. init-vec size 0 -1 type def-val direction nil)))
--- test/hamakar/circular_buffer_test.clj
+++ test/hamakar/circular_buffer_test.clj
@@ -4,24 +4,24 @@
 
 (deftest test-circular-buffer-creation
   (testing "The direction defines correct order of items"
-     (are [a b] (= a b)
-          (into (cbuf 4 :direction :left)  [1 2]) [nil nil 1 2]
-          (into (cbuf 4 :direction :right) [1 2]) [2 1 nil nil]
-          (into (cbuf 4 :direction :left)  [1 2 3 4 5]) [2 3 4 5]
-          (into (cbuf 4 :direction :right) [1 2 3 4 5]) [5 4 3 2]))
+    (are [a b] (= a b)
+      (into (cbuf 4 :direction :left ) [1 2]) [nil nil 1 2]
+      (into (cbuf 4 :direction :right ) [1 2]) [2 1 nil nil]
+      (into (cbuf 4 :direction :left ) [1 2 3 4 5]) [2 3 4 5]
+      (into (cbuf 4 :direction :right ) [1 2 3 4 5]) [5 4 3 2]))
   (testing "A buffer of specific type is backed with a right data structure"
-    (let [size 5 cb (cbuf size)] 
-     (is (and (== size (count cb))
-              (instance? clojure.lang.IPersistentVector (.items cb)))))
+    (let [size 5 cb (cbuf size)]
+      (is (and (== size (count cb))
+            (instance? clojure.lang.IPersistentVector (.items cb)))))
     (are [x size] (and (== size (count x)) (instance? clojure.core.Vec (.items x)))
-         (cbuf 1 :type :boolean) 1
-         (cbuf 2 :type :byte) 2
-         (cbuf 3 :type :short) 3
-         (cbuf 4 :type :int) 4
-         (cbuf 5 :type :long) 5
-         (cbuf 6 :type :float) 6
-         (cbuf 7 :type :double) 7
-         (cbuf 8 :type :char) 8))
+      (cbuf 1 :type :boolean ) 1
+      (cbuf 2 :type :byte ) 2
+      (cbuf 3 :type :short ) 3
+      (cbuf 4 :type :int ) 4
+      (cbuf 5 :type :long ) 5
+      (cbuf 6 :type :float ) 6
+      (cbuf 7 :type :double ) 7
+      (cbuf 8 :type :char ) 8))
   )
 
 (deftest test-counted-interface
@@ -30,96 +30,93 @@
           cb-1 (into cb-0 [1 2 3 4 5 6 7])
           cb-2 (conj cb-0 8)]
       (are [x] (= 3 (count x))
-           cb-0
-           cb-1
-           cb-2))))
+        cb-0
+        cb-1
+        cb-2))))
 
 (deftest test-indexed-interface
-  (let [cb-l (into (cbuf 3 :direction :left)  [1 2 3 4 5 6 7])
-        cb-r (into (cbuf 3 :direction :right) [1 2 3 4 5 6 7])]
-	  (testing "nth always returns a value"
-       (are [cb pos val] (= val (nth cb pos))
-             cb-l 0 5
-             cb-l 1 6
-             cb-l 2 7
-             cb-l 3 5
-             cb-l 4 6
-             cb-l 5 7
-             cb-r 0 7
-             cb-r 1 6
-             cb-r 2 5
-             cb-r 3 7
-             cb-r 4 6
-             cb-r 5 5))
-	  (testing "nth with not-found returns not-found when the index out of bounds"
-	     (are [cb pos val] (= val (nth cb pos :not-found))
-	          cb-l 0 5
-	          cb-l 1 6
-	          cb-l 2 7
-	          cb-l 3 :not-found
-	          cb-l 4 :not-found
-            cb-r 0 7
-	          cb-r 1 6
-	          cb-r 2 5
-	          cb-r 3 :not-found
-	          cb-r 4 :not-found))))
+  (let [cb-l (into (cbuf 3 :direction :left ) [1 2 3 4 5 6 7])
+        cb-r (into (cbuf 3 :direction :right ) [1 2 3 4 5 6 7])]
+    (testing "nth always returns a value"
+      (are [cb pos val] (= val (nth cb pos))
+        cb-l 0 5
+        cb-l 1 6
+        cb-l 2 7
+        cb-l 3 5
+        cb-l 4 6
+        cb-l 5 7
+        cb-r 0 7
+        cb-r 1 6
+        cb-r 2 5
+        cb-r 3 7
+        cb-r 4 6
+        cb-r 5 5))
+    (testing "nth with not-found returns not-found when the index out of bounds"
+      (are [cb pos val] (= val (nth cb pos :not-found ))
+        cb-l 0 5
+        cb-l 1 6
+        cb-l 2 7
+        cb-l 3 :not-found cb-l 4 :not-found cb-r 0 7
+        cb-r 1 6
+        cb-r 2 5
+        cb-r 3 :not-found cb-r 4 :not-found ))))
 
 (deftest test-persistent-collection-interface
   (let [cb-0 (into (cbuf 3) [1 2 3 4 5])
         cb-1 (into (cbuf 3) [1 2 3 4 6])
         cb-2 (into (cbuf 4) [1 2 3 4 5])
-        cb-3 (into (cbuf 2 :type :boolean) [true false true])
-        cb-4 (into (cbuf 3 :direction :right) [1 2 3 4 5])]
-	  (testing "="
-	       (are [x] (= x cb-0)
-	            cb-0
-	            [3 4 5]
-	            (list 3 4 5)
-	            (range 3 6)))
-	  (testing "cons"
-	      (are [result input] (= result (.cons input :foo))
-	           [4 5 :foo] cb-0
-             [:foo 5 4] cb-4))
+        cb-3 (into (cbuf 2 :type :boolean ) [true false true])
+        cb-4 (into (cbuf 3 :direction :right ) [1 2 3 4 5])]
+    (testing "="
+      (are [x] (= x cb-0)
+        cb-0
+        [3 4 5]
+        (list 3 4 5)
+        (range 3 6)))
+    (testing "cons"
+      (are [result input] (= result (.cons input :foo ))
+        [4 5 :foo ] cb-0
+        [:foo 5 4] cb-4))
     (testing "empty"
-        (are [a b val] (and (= (count a) (count b)) (zero? (.start b)) (every? #(= val %) b))
-             cb-0 (empty cb-0) nil
-             cb-3 (empty cb-3) false
-             cb-4 (empty cb-4) nil))
+      (are [a b val] (and (= (count a) (count b)) (zero? (.start b)) (every? #(= val %) b))
+        cb-0 (empty cb-0) nil
+        cb-3 (empty cb-3) false
+        cb-4 (empty cb-4) nil))
     (testing "equiv"
-        (are [a b] (true? (.equiv a b))
-             cb-0 cb-0
-             cb-1 cb-1
-             cb-2 cb-2
-             cb-3 cb-3
-             cb-4 cb-4)
-        (are [a b] (false? (.equiv a b))
-             cb-0 cb-1
-             cb-0 cb-2
-             cb-1 cb-0
-             cb-2 cb-0
-             cb-1 cb-2
-             cb-1 cb-3
-             cb-0 cb-4))))
+      (are [a b] (true? (.equiv a b))
+        cb-0 cb-0
+        cb-1 cb-1
+        cb-2 cb-2
+        cb-3 cb-3
+        cb-4 cb-4)
+      (are [a b] (false? (.equiv a b))
+        cb-0 cb-1
+        cb-0 cb-2
+        cb-1 cb-0
+        cb-2 cb-0
+        cb-1 cb-2
+        cb-1 cb-3
+        cb-0 cb-4))))
 
 (deftest test-persistent-stack-interface
   (let [cb-ob-1 (into (cbuf 2) [1 2 3 4])
-        cb-ob-2 (into (cbuf 2 :direction :right) [1 2 3 4])
-        cb-int  (into (cbuf 3 :type :int) [1 2 3])
+        cb-ob-2 (into (cbuf 2 :direction :right ) [1 2 3 4])
+        cb-int (into (cbuf 3 :type :int ) [1 2 3])
         cb-bool (into (cbuf 4 :type :boolean :default true) [true false true])]
     (testing "peek"
-       (are [cb v] (= v (peek cb))
-            cb-ob-1 4
-            cb-ob-2 4))
+      (are [cb v] (= v (peek cb))
+        cb-ob-1 4
+        cb-ob-2 4))
     (testing "pop"
-       (are [a b] (= a b)
-            [nil 3] (pop cb-ob-1)
-            [3 nil] (pop cb-ob-2)
-            [0 1 2] (pop cb-int)
-            [true true true false] (pop cb-bool)
-            [nil nil] (pop (pop cb-ob-1))))
+      (are [a b] (= a b)
+        [nil 3] (pop cb-ob-1)
+        [3 nil] (pop cb-ob-2)
+        [0 1 2] (pop cb-int)
+        [true true true false] (pop cb-bool)
+        [nil nil] (pop (pop cb-ob-1))))
     (testing "Combinations of conj and pop"
-       (are [a b] (= a b)
-            [3 4] (conj (pop (conj (pop cb-ob-1) 8)) 4)
-            [4 3] (conj (pop (conj (pop cb-ob-2) 8)) 4)
-            [0 1 8] (conj (pop (pop cb-int)) 8)
-            [true true true true] (pop (pop (pop cb-bool)))))))
+      (are [a b] (= a b)
+        [3 4] (conj (pop (conj (pop cb-ob-1) 8)) 4)
+        [4 3] (conj (pop (conj (pop cb-ob-2) 8)) 4)
+        [0 1 8] (conj (pop (pop cb-int)) 8)
+        [true true true true] (pop (pop (pop cb-bool)))))))
